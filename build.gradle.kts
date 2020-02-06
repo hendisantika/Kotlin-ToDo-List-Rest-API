01d052356bab352e2f6d7e2b03945b6afd19b0c7
Hendi Santika <hendisantika@gmail.com>
2020-02-07 06:12:30 +0700

    #3 - Add required libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.postgresql:postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
