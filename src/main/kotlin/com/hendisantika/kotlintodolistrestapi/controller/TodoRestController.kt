7cad0363fbf20b14b811dce9f5ef0b1916a29afe
Hendi Santika <hendisantika@gmail.com>
2020-02-07 06:23:10 +0700

    #11 - Add getTodoById API in TodoRestController

--- src/main/kotlin/com/hendisantika/kotlintodolistrestapi/controller/TodoRestController.kt
+++ src/main/kotlin/com/hendisantika/kotlintodolistrestapi/controller/TodoRestController.kt
@@ -1,9 +1,12 @@
 package com.hendisantika.kotlintodolistrestapi.controller
 
+import com.hendisantika.kotlintodolistrestapi.entity.Todo
 import com.hendisantika.kotlintodolistrestapi.repository.TodoRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +22,9 @@ import org.springframework.web.bind.annotation.RestController
 class TodoRestController(val todoRepository: TodoRepository) {
     @GetMapping
     fun getTodos() = todoRepository.findAll()
+
+    @GetMapping(path = [("/{todoId}")])
+    fun getTodo(@PathVariable("todoId") todoId: Long?): Optional<Todo>? {
+        return todoRepository.findById(todoId)
+    }
 }
