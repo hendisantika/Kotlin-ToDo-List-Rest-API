7a573c87520828d0bc7fc3bbcecf370d5bc62ee3
Hendi Santika <hendisantika@gmail.com>
2020-02-07 06:16:10 +0700

    #5 - Add Todo Entity

---
+++ src/main/kotlin/com/hendisantika/kotlintodolistrestapi/entity/Todo.kt
@@ -0,0 +1,29 @@
+package com.hendisantika.kotlintodolistrestapi.entity
+
+import javax.persistence.*
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kotlin-todo-list-rest-api
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/02/20
+ * Time: 06.15
+ */
+@Entity
+@Table(name = "todo")
+data class Todo {
+
+    /* GenerationType.IDENTITY is for databases like Postgres
+        that use sequence tables instead of auto-increment
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    var id: Long? = null
+
+    var title: String? = null
+    var description: String? = null
+    var finished: Boolean? = false
+}
