eaf27e251435e2087a22c42d8771963ffb1d47b5
Hendi Santika <hendisantika@yahoo.co.id>
2020-06-25 06:02:44 +0700

    #33 - Upgraded into Spring Boot 2.3.7.RELEASE version
    #33 - Upgraded into Spring Boot 2.3.7.RELEASE version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.6.RELEASE"
+    id("org.springframework.boot") version "2.3.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.flywaydb.flyway") version "6.2.1"
     kotlin("jvm") version "1.3.61"
