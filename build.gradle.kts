fd61fa1e419787a435a8e81a4e9c7a99176dc8ee
Hendi Santika <hendisantika@yahoo.co.id>
2025-04-16 08:50:02 +0700

    feat: Upgraded into Spring Boot 3.3.8 version 🫸🌀✏️📗🐧🐳⬆
    feat: Upgraded into Spring Boot 3.3.8 version 🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.3.7"
+    id("org.springframework.boot") version "3.3.8"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.flywaydb.flyway") version "10.19.0"
     kotlin("jvm") version "2.0.20"
